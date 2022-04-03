d6e075557a927b7e5e90203aa408c243ac65ad19
Alvaro Gaiotti <gttlvr@gmail.com>
2022-04-03 23:54:07 +0200

    Implement FFI for Bo Haglund's Double Dummy Solver. src/lib.rs via bindgen. Must be cleaned.

---
+++ build.rs
@@ -0,0 +1,7 @@
+fn main() {
+    println!("cargo:rustc-link-lib=dylib=libdds");
+    println!("cargo:rustc-link-search=native=/home/alvaro/Programming/workspace-rust/rusty-dealer/libdds.so");
+    println!(
+        "cargo:rustc-link-arg=-Wl,/home/alvaro/Programming/workspace-rust/rusty-dealer/libdds.so"
+    );
+}
