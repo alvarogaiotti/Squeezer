3ee43fb2871e81a13b0c503c70dcc924b024dd6a
thevava <gttlvr@gmail.com>
2023-02-20 00:58:44 +0100

    Implement Evaluator::new() more idiomatically using iterators

---
+++ src/evaluator.rs
@@ -0,0 +1,39 @@
+use crate::prelude::*;
+
+pub struct Evaluator {
+    evaluator: Box<dyn Fn(&Cards) -> u8>,
+}
+
+impl Evaluator {
+    pub fn new(values: &[u8]) -> Self {
+        let mut vals = [0u8; 13];
+        // Starting from 13 so we populate vals with rank indexes
+        // using enumerate to take the values that comes in when
+        // new is calle with the standard parameters like values: &[4,3,2,1]
+        //                                                          A,K,Q,J,...
+        (13..13 - values.len()).enumerate().for_each(
+            |(position_in_values, rank_equivalent_for_vals)| {
+                vals[rank_equivalent_for_vals] = values[position_in_values]
+            },
+        );
+        Self {
+            evaluator: Box::new(move |x: &Cards| {
+                x.into_iter()
+                    .map(|y| vals[y.rank() as usize - 2])
+                    .sum::<u8>()
+            }),
+        }
+    }
+    pub fn evaluate(&self, hand: &Cards) -> u8 {
+        (self.evaluator)(hand)
+    }
+}
+
+#[cfg(test)]
+#[test]
+fn evaluate_correctly_test() {
+    let hcp = Evaluator::new(&[4u8, 3u8, 2u8, 1u8]);
+    let mut deck = Cards::ALL;
+    let hand = deck.pick(13).unwrap();
+    assert_eq!(hcp.evaluate(&hand), hand.high_card_points() as u8);
+}
