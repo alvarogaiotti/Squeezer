3fc28b9f3884fb275d6374ed789f57ed7f0f955f
thevava <gttlvr@gmail.com>
2023-04-15 11:07:54 +0200

    optionally link to Windows dll

--- build.rs
+++ build.rs
@@ -1,5 +1,11 @@
 fn main() {
+    #[cfg(target_os = "linux")]
     println!(
         "cargo:rustc-link-arg=-Wl,/home/alvaro/Programming/workspace-rust/rusty-dealer/libdds.so"
+    );
+
+    #[cfg(target_os = "windows")]
+    println!(
+        "cargo:rustc-link-arg=C:\\Users\\gttlv\\RustProjects\\bridge_bro\\rusty_dealer\\dds.lib"
     );
 }
