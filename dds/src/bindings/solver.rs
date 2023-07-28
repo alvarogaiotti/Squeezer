056608f13c3cf7ecba7d0966cafd47e5e4376242
thevava <gttlvr@gmail.com>
2023-07-29 00:47:09 +0200

    Enforced constraints via the type system

--- dds/src/bindings/solver.rs
+++ dds/src/bindings/solver.rs
@@ -1,23 +1,89 @@
-use crate::RawDDS;
-
 use super::{
     ddsffi::SolveBoard,
     deal::{AsDDSDeal, DDSDealBuilder, DDSDealConstructionError},
     future_tricks::FutureTricks,
     AsDDSContract,
 };
+use crate::RawDDS;
+use std::num::NonZeroI32;
 
-pub trait BridgeSolver<E: std::error::Error> {
-    fn dd_tricks<D: AsDDSDeal, C: AsDDSContract>(&self, deal: &D, contract: &C) -> Result<u8, E>;
+enum ThreadIndex {
+    Auto,
+    NumThreads(NonZeroI32),
 }
-pub struct DDSSolver {}
 
-impl BridgeSolver<DDSDealConstructionError> for DDSSolver {
+impl From<ThreadIndex> for std::ffi::c_int {
+    fn from(value: ThreadIndex) -> Self {
+        match value {
+            ThreadIndex::Auto => 0,
+            ThreadIndex::NumThreads(value) => value.into(),
+        }
+    }
+}
+
+enum Target {
+    MaxTricks,
+    LegalNoScore,
+    Goal(NonZeroI32),
+}
+
+impl From<Target> for std::ffi::c_int {
+    fn from(value: Target) -> Self {
+        match value {
+            Target::MaxTricks => -1,
+            Target::LegalNoScore => 0,
+            Target::Goal(goal) => std::ffi::c_int::max(13, goal.into()),
+        }
+    }
+}
+
+enum Solutions {
+    Best,
+    AllOptimal,
+    AllLegal,
+}
+
+impl From<Solutions> for std::ffi::c_int {
+    fn from(value: Solutions) -> Self {
+        match value {
+            Solutions::Best => 1,
+            Solutions::AllOptimal => 2,
+            Solutions::AllLegal => 3,
+        }
+    }
+}
+
+enum Mode {
+    Auto,
+    AutoSearchAlways,
+    Always,
+}
+
+impl From<Mode> for std::ffi::c_int {
+    fn from(value: Mode) -> Self {
+        match value {
+            Mode::Auto => 0,
+            Mode::AutoSearchAlways => 1,
+            Mode::Always => 2,
+        }
+    }
+}
+
+pub trait BridgeSolver {
     fn dd_tricks<D: AsDDSDeal, C: AsDDSContract>(
         &self,
         deal: &D,
         contract: &C,
-    ) -> Result<u8, DDSDealConstructionError> {
+    ) -> Result<u8, Box<dyn std::error::Error>>;
+}
+pub struct DDSSolver {}
+
+impl BridgeSolver for DDSSolver {
+    fn dd_tricks<D: AsDDSDeal, C: AsDDSContract>(
+        &self,
+        deal: &D,
+        contract: &C,
+    ) -> Result<u8, Box<dyn std::error::Error>> {
         let (trump, first) = contract.as_dds_contract();
         let c_deal = DDSDealBuilder::new()
             .trump(trump.try_into()?)
@@ -26,7 +92,17 @@ impl BridgeSolver<DDSDealConstructionError> for DDSSolver {
             .build()?;
         let future_tricks = FutureTricks::new();
         let futp = &mut future_tricks.get_raw();
-        unsafe { SolveBoard(c_deal.get_raw(), -1, 1, 1, futp, 0) };
+        let result;
+        unsafe {
+            result = SolveBoard(
+                c_deal.get_raw(),
+                Target::MaxTricks.into(),
+                Solutions::Best.into(),
+                Mode::AutoSearchAlways.into(),
+                futp,
+                ThreadIndex::Auto.into(),
+            )
+        };
         Ok(13 - future_tricks.score()[0] as u8)
     }
 }
