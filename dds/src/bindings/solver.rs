18787d0c76c8e943c2fb082efa81ab7a619bb130
thevava <gttlvr@gmail.com>
2023-08-27 17:53:38 +0200

    Add solver implementation

--- dds/src/bindings/solver.rs
+++ dds/src/bindings/solver.rs
@@ -2,72 +2,10 @@ use super::{
     ddsffi::SolveBoard,
     deal::{AsDDSDeal, DDSDealBuilder, DDSDealConstructionError},
     future_tricks::FutureTricks,
-    AsDDSContract,
+    AsDDSContract, Mode, Solutions, Target, ThreadIndex,
 };
 use crate::{DDSError, RawDDS};
 use std::num::NonZeroI32;
-
-enum ThreadIndex {
-    Auto,
-    NumThreads(NonZeroI32),
-}
-
-impl From<ThreadIndex> for std::ffi::c_int {
-    fn from(value: ThreadIndex) -> Self {
-        match value {
-            ThreadIndex::Auto => 0,
-            ThreadIndex::NumThreads(value) => value.into(),
-        }
-    }
-}
-
-enum Target {
-    MaxTricks,
-    LegalNoScore,
-    Goal(NonZeroI32),
-}
-
-impl From<Target> for std::ffi::c_int {
-    fn from(value: Target) -> Self {
-        match value {
-            Target::MaxTricks => -1,
-            Target::LegalNoScore => 0,
-            Target::Goal(goal) => std::ffi::c_int::max(13, goal.into()),
-        }
-    }
-}
-
-enum Solutions {
-    Best,
-    AllOptimal,
-    AllLegal,
-}
-
-impl From<Solutions> for std::ffi::c_int {
-    fn from(value: Solutions) -> Self {
-        match value {
-            Solutions::Best => 1,
-            Solutions::AllOptimal => 2,
-            Solutions::AllLegal => 3,
-        }
-    }
-}
-
-enum Mode {
-    Auto,
-    AutoSearchAlways,
-    Always,
-}
-
-impl From<Mode> for std::ffi::c_int {
-    fn from(value: Mode) -> Self {
-        match value {
-            Mode::Auto => 0,
-            Mode::AutoSearchAlways => 1,
-            Mode::Always => 2,
-        }
-    }
-}
 
 pub trait BridgeSolver {
     fn dd_tricks<D: AsDDSDeal, C: AsDDSContract>(
