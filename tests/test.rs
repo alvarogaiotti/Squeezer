6eac7930f3e1fe6b6bdf0bbf046c69a0f0434452
thevava <gttlvr@gmail.com>
2023-08-27 17:54:48 +0200

    Add integration test directory

---
+++ tests/test.rs
@@ -0,0 +1,18 @@
+use rusty_dealer::*;
+
+#[test]
+fn main_to_be() {
+    let predeal = Hand::from_str("SQJ763HT2CJT9832").unwrap();
+    let mut south_shapes = Shapes::new();
+    south_shapes
+        .add_shape(ShapeDescriptor::new("(4333)"))
+        .unwrap();
+    let south_specs = HandDescriptor::new(vec![HandType::new(south_shapes, HcpRange::new(24, 37))]);
+    let mut builder = DealerBuilder::new();
+    builder
+        .predeal(Seat::North, predeal)
+        .with_hand_specification(Seat::South, south_specs);
+    let dealer = builder.build().unwrap();
+    let mut deal = dealer.deal().unwrap();
+    deal.pbn();
+}
