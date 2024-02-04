758c31829d78f765dac2fbabdee1e144aed6453d
thevava <gttlvr@gmail.com>
2024-02-04 10:13:48 +0100

    Implement a new test

--- tests/test.rs
+++ tests/test.rs
@@ -1,17 +1,39 @@
-
+use dds::dd_score;
 use squeezer::*;
+use std::collections::HashMap;
 
 #[test]
 fn main_to_be() {
-    let predeal = Hand::from_str("SQJ763HT2CJT9832").unwrap();
+    let predeal = Hand::from_str("SQ63H542DK42C8763").unwrap();
     let mut south_shapes = Shapes::new();
-    south_shapes.add_shape(String::from("(4333)")).unwrap();
-    let south_specs = HandDescriptor::new(vec![HandType::new(south_shapes, HcpRange::new(24, 37))]);
+    south_shapes.add_shape("3262").unwrap();
+    south_shapes.add_shape("2362").unwrap();
+    south_shapes.add_shape("2263").unwrap();
+    south_shapes.add_shape("2272").unwrap();
+
+    let south_specs = HandDescriptor::new(vec![HandType::new(
+        Shape::Custom(south_shapes),
+        HcpRange::new(18, 20),
+    )]);
+    let mut hand_builder = HandTypeBuilder::new();
+    hand_builder
+        .with_longest(Suit::Spades)
+        .with_range(8, 15)
+        .remove_shape("7xxx")
+        .and_then(|buildr| {
+            buildr
+                .remove_shape("8xxx")
+                .and_then(|buildr| buildr.remove_shape("9xxx"))
+        })
+        .unwrap();
+    let west_spec = hand_builder.build();
     let mut builder = DealerBuilder::new();
     builder
         .predeal(Seat::North, predeal)
-        .with_hand_specification(Seat::South, south_specs);
+        .with_hand_specification(Seat::South, south_specs)
+        .with_hand_specification(Seat::West, HandDescriptor::new(vec![west_spec]))
+        .with_function(|hands: &Hands| hands.south().spades().high_card_points() > 2);
     let dealer = builder.build().unwrap();
-    let mut deal = dealer.deal().unwrap();
-    deal.pbn();
+    let deal = dealer.deal().unwrap();
+    assert!(deal.south().diamonds().len() >= 6);
 }
