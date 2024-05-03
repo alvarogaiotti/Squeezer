1fc68f08c99e90f97eb973479f72c74c53dbfb13
Alvaro Gaiotti <gttlvr@gmail.com>
2024-05-03 18:43:22 +0200

    Correct

--- src/analyse_performance.rs
+++ src/analyse_performance.rs
@@ -1,6 +1,7 @@
 use dds::*;
 use crate::prelude::*;
 use itertools::*;
+use std::cmp::Ordering;
 
 pub struct TraceSolved {
     pub trace : PlaySequence,
@@ -30,14 +31,14 @@ impl From<u8> for TrickPosition {
 /// Analyse the player performace based on the DDS play analysis
 pub fn analyse_player_performance(player: Seat, contract: Contract, play_result_trace: TraceSolved) {
     let TraceSolved {trace, results} = play_result_trace;
-    let mut tricks_iterator = results.results.into_iter().peek();
+    let mut tricks_iterator = results.into_iter().peek();
     let mut correct_played = 0;
     let mut total_played = 0;
     let mut starting_position;
     if contract.leader() == player {
         let optimal = tricks_iterator.next();
         if let Some(tricks) = tricks_iterator.peek() {
-            correct_played *= optimal == tricks;
+            correct_played += optimal == tricks as usize;
         } else {
             unreachable!();
         }
@@ -45,8 +46,8 @@ pub fn analyse_player_performance(player: Seat, contract: Contract, play_result_
     } else {
         starting_position = (4 - contract.leader() as u8 + player as u8).into();
     }
-    
-    for (trick, dd_res) in (&trace.into_iter().chunks(4)).zip(&tricks_iterator.chunks(4)) {
+
+    for (trick, dd_res) in trace.into_iter().chunks(4).into_iter().zip(&tricks_iterator.chunks(4).into_iter()) {
         if &dd_res.nth(starting_position as usize) >= &dd_res.nth(starting_position as usize - 1) {
             correct_played += 1
         } 
@@ -55,17 +56,17 @@ pub fn analyse_player_performance(player: Seat, contract: Contract, play_result_
 }
 
 fn max_for_suit(trump: Option<Suit>) -> impl Fn(Card, Card) -> bool {
-    let default = |c1, c2| if c1.suit() > c2.suit() {
-        true
-    } else if c1.suit() == c2.suit() {
-        c1.rank() > c2.rank()
+    let default = |c1: Card, c2: Card| if c1.suit() != c2.suit() {
+        // Remember that suit are stored in reverse order as a type
+        // e.g. Spades < Hearts
+        c1.suit() < c2.suit()
     } else {
-        false
+        c1.rank() > c2.rank()
     };
     if let Some(trump) = trump {
         |c1, c2| if c1.suit() == trump {
             if c2.suit() != trump {
-                true
+                Ordering::Greater
             } else {
                 c1.rank() > c2.rank()
             }
@@ -75,8 +76,4 @@ fn max_for_suit(trump: Option<Suit>) -> impl Fn(Card, Card) -> bool {
     } else {
         default
     }
-}
-
-fn winner(leader: TrickPosition, sequence: Iterator<Item=Card>, trump: Option<Suit>) -> Option<TrickPosition> {
-    return sequence.max_by(max_for_suit(trump))
 }
